02f9635fcd9c2dedecfddac18d8603c227bcb2e3
Genis Riera Perez <genis.riera.perez@gmail.com>
2016-06-27 23:52:46 +0200

    Add new environment for ultrabook + new script
    The new script for applying an environment configuration is now located
    at root level of repository, and manages all environments.

---
+++ apply_environ.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+if [ $# -eq 0 ] ; then
+    environ=$HOSTNAME
+elif [ $# -eq 1 ] ; then
+    environ=$1
+else
+    echo "Usage: $0 [environ_directory]"
+    exit 1
+fi
+
+environ_path="${PWD}/${environ}"
+if ! [ -d $environ_path ] ; then
+    echo "$environ environment directory does not exist"
+    exit 2
+fi
+
+record_file="${environ_path}/dotfiles_record.txt"
+while read -r line ; do
+    orig_file="$(echo $line | cut -d ' ' -f2)"
+    new_file="${environ_path}/$(echo $line | cut -d ' ' -f1)"
+    rm -rf $orig_file
+    ln -s $new_file $orig_file
+done < $record_file
--- debian-home/my_apply_environ.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-while read -r line ; do
-    orig_file="$(echo $line | cut -d ' ' -f2)"
-    new_file="${PWD}/$(echo $line | cut -d ' ' -f1)"
-    rm -rf $orig_file
-    ln -s $new_file $orig_file
-done < dotfiles_record.txt
