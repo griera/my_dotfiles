2b824cf48a4b93bec4141995867b976df9f65539
Genis Riera Perez <genis.riera.perez@gmail.com>
2019-12-12 13:01:17 +0100

    Add virsh bash completion
    from https://github.com/LuyaoHuang/virsh-bash-completion

--- apply_environ.sh
+++ apply_environ.sh
@@ -20,7 +20,11 @@ while read -r line ; do
     orig_file="$(echo $line | cut -d ' ' -f2)"
     new_file="${environ_path}/$(echo $line | cut -d ' ' -f1)"
     rm -rf $orig_file
-    ln -s $new_file $orig_file
+    if [ -z "${orig_file##*$HOME*}" ] ; then
+        ln -s $new_file $orig_file
+    else
+        sudo ln -s $new_file $orig_file
+    fi
 done < $record_file
 
 exit
